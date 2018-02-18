3948b45c8fde89f4755acd6a17658e3132a9a553
ghizzo01 <36126125+ghizzo01@users.noreply.github.com>
2018-02-18 11:04:07 +0100

    aggiunta data aggiornamento

--- compiti.js
+++ compiti.js
@@ -1,11 +1,17 @@
 var xmlHttp = new XMLHttpRequest();
 xmlHttp.onreadystatechange = function() {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        var html = "<ul>";
+        var html = "";
 
         var interpretato = JSON.parse(xmlHttp.responseText);
-        for(var i = 0; i < interpretato.length; i++) {
-            var giorno = interpretato[i];
+
+        var ultimo_aggiornamento = interpretato["ultimo_aggiornamento"];
+        html = html + '<p class="aggiornamento">'+"ultimo aggiornamento: " + ultimo_aggiornamento + '</p>';
+
+        var giorni = interpretato['giorni'];
+        html = html + "<ul>";
+        for(var i = 0; i < giorni.length; i++) {
+            var giorno = giorni[i];
             var nome_giorno = giorno['giorno'];
             html = html + "<li>" +'<p class="giorno"'+ '</p>' + nome_giorno ;
             html = html + "<ul>";
@@ -18,8 +24,6 @@ xmlHttp.onreadystatechange = function() {
                var nome_compito = compito['compito'];
                html = html + "<li>" + '<p class="materie">' + nome_materia+ "</p>";
                html = html + '<p class="compito">' + nome_compito + "</p>" + "</li>";
-
-
             }
 
             html = html + "</ul></li>";
